d97fb05179d5b3e8c52024a8020bd60ee93fc574
weihanli <ben121011@126.com>
2016-11-20 18:58:49 +0800

    增加添加目录功能

--- md2htmlTool/Form1.cs
+++ md2htmlTool/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -6,18 +8,47 @@ namespace md2htmlTool
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// 标题正则
+        /// </summary>
+        private const string TitlePattern = @"^(?<level>#{1,6})[\s\t](?<title>.+)";
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void txtMdText_TextChanged(object sender, EventArgs e)
+        private void txtMdText_TextChanged(object sender , EventArgs e)
         {
-            string mdText = txtMdText.Text;
-            if (String.IsNullOrEmpty(mdText))
+            if (String.IsNullOrEmpty(txtMdText.Text))
             {
                 return;
             }
+            MatchCollection matches = Regex.Matches(txtMdText.Text , TitlePattern , RegexOptions.Multiline | RegexOptions.ExplicitCapture);
+            if (matches.Count > 0)
+            {
+                StringBuilder sbText = new StringBuilder();
+                int level = 0;
+                int[] titleIndex = new int[6];
+                string title;
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    Match item = matches[i];                    
+                    level = item.Groups["level"].Value.Length;
+                    titleIndex[level - 1]++;
+                    for (int j = level; j < 6; j++)
+                    {
+                        if (titleIndex[j]!=0)
+                        {
+                            titleIndex[j] = 0;
+                        }                        
+                    }
+                    title = item.Groups["title"].Value.GenerateMenuItem(titleIndex);
+                    sbText.AppendFormat("{0}{1}" , "\t".Multiple(level - 1) + title , Environment.NewLine);
+                }
+                txtMdText.Text = sbText+ txtMdText.Text;
+            }
+            string mdText = txtMdText.Text;
             MarkdownSharp.Markdown md = new MarkdownSharp.Markdown();
             string htmlText = md.Transform(mdText);
             txtHtmlText.Text = htmlText;
@@ -26,20 +57,9 @@ namespace md2htmlTool
                 Clipboard.SetText(htmlText);
                 MessageBox.Show("html copied");
             }
-            catch (System.Runtime.InteropServices.ExternalException ex)
-            {
-                throw ex;
-            }
-            catch (ThreadStateException ex)
-            {
-                throw ex;
-            }
             catch (Exception ex)
             {
-                throw ex;
-            }
-            finally
-            {
+                MessageBox.Show("发生异常，异常信息："+ex.Message);
             }
         }
     }
---
+++ md2htmlTool/Utility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace md2htmlTool
+{
+    public static class Utility
+    {
+        public static string Multiple(this String str , int repeatCount)
+        {
+            if (String.IsNullOrEmpty(str) || repeatCount <= 0)
+            {
+                return "";
+            }
+            StringBuilder sbText = new StringBuilder();
+            for (int i = 0; i < repeatCount; i++)
+            {
+                sbText.Append(str);
+            }
+            return sbText.ToString();
+        }
+
+        public static string GenerateMenuItem(this String title , int[] titleIndex)
+        {
+            StringBuilder sbText = new StringBuilder();
+            int i = 0;
+            while (titleIndex[i] != 0)
+            {
+                sbText.Append(titleIndex[i]);
+                sbText.Append('.');
+                i++;
+            }
+            string itemIndex = sbText.ToString().Substring(0 , sbText.Length - 1);
+            sbText.Clear();
+            sbText.AppendFormat("{0} {1}" , itemIndex , title);
+            return sbText.ToString();
+        }
+    }
+}
